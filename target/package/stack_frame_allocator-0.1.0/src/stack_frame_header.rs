d4b228ee825d4340be6e6fa07da2df5fa8ed62bf
IrrelevantGaymer <101901cora@gmail.com>
2024-10-09 10:54:34 -0700

    target fix

--- target/package/stack_frame_allocator-0.1.0/src/stack_frame_header.rs
+++
@@ -1,6 +0,0 @@
-/// A block of data placed in memory blocks to keep track of active stack frames
-/// and help traverse the stack
-pub(crate) struct StackFrameHeader<'sf> {
-    pub(crate) previous_frame: Option<&'sf StackFrameHeader<'sf>>,
-    pub(crate) current_frame_ptr: *mut u8
-}
