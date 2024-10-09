1c61387f79363f0c716c7b610cc7a8233b3f1bfc
IrrelevantGaymer <101901cora@gmail.com>
2024-10-09 11:51:09 -0700

    gitignore

--- target/package/stack_frame_allocators-0.1.0/target/debug/deps/stack_frame_allocator-b8344bc7bc86ec09.d
+++
@@ -1,5 +0,0 @@
-/home/river/Desktop/Projects/Sugar/stack_frame_allocator/target/debug/deps/libstack_frame_allocator-b8344bc7bc86ec09.rmeta: src/main.rs
-
-/home/river/Desktop/Projects/Sugar/stack_frame_allocator/target/debug/deps/stack_frame_allocator-b8344bc7bc86ec09.d: src/main.rs
-
-src/main.rs:
